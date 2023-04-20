10e33f57b0300d61b85c3da3c0df93539f670298
Zaunzi <theaidanb@gmail.com>
2023-04-20 16:34:32 -0400

    lezgo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,15 +10,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={"polygon"}>
       <Head>
-        <title>thirdweb Edition Drop Minting Customizable Page</title>
+        <title>Secret Stash</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
-          content="Learn How To Use Thirdweb's Edition Drop contract and create a customizable Edition Drop minting page"
+          content="Secret Stash Minting dApp - Discover a world of digital treasures hidden within the Secret Stash! Unlock the gateway to an ever-evolving collection of unique NFT collectibles, blending art, utility, and rarity."
         />
         <meta
           name="keywords"
-          content="Thirdweb, thirdweb Edition drop, how to make thirdweb nft drop, how to make nft collection thirdweb"
+          content="Secret Stash Minting dApp - Discover a world of digital treasures hidden within the Secret Stash! Unlock the gateway to an ever-evolving collection of unique NFT collectibles, blending art, utility, and rarity."
         />
       </Head>
       <Component {...pageProps} />
